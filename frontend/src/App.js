f42ed64d86c827f4b6d32f86d1b76a16e182aa8b
muazam-cmd <muazamshahid7@gmail.com>
2021-12-21 19:30:41 +0500

    Header and footer with reactBootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <h1>wellcome to pro shop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>wellcome to proShop</h1>
+        </Container>
+      </main>
+      <Footer />
+
     </>
   );
 }
