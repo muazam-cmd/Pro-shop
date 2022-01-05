ebb9b5b6535bc69059725374f379e78268342219
muazam-cmd <muazamshahid7@gmail.com>
2022-01-06 02:01:34 +0500

    products details Reducer and actions

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
+import { listProducts } from '../actions/productAction'
+
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
+
+
     return (
         <>
-            <Row>
+            <h1>Latest Products</h1>
+            {loading ? <Loader /> : error ? <Message variant='danger' /> : <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+            </Row>}
+
         </>
     )
 }
